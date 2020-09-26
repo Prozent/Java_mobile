7d69dee79e9daa5addeb07cc04df62a9c4f042e5
Dima-byte <59562902+Dima-byte@users.noreply.github.com>
2020-09-26 15:37:48 +0700

    Последний комит, добавил поле входа в акаунт, освоил методику подключение других лаяутов

--- app/src/main/java/com/example/tets/MainActivity.java
+++ app/src/main/java/com/example/tets/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread welcomeThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(10000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(MainActivity.this,Skreen_sign_in.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
     }
 }
